e57e3a4c3396a1b3c3128ed7cca3863be331ae42
Kuba Jaroszewski <jakub.jaroszewski@gmail.com>
2022-06-07 21:55:01 -0500

    Add directory structure with some mock implementations

---
+++ src/ui/MenuOption.java
@@ -0,0 +1,35 @@
+package ui;
+
+/**
+ * Represents an option selectable in a menu
+ */
+public class MenuOption {
+    private final String caption;
+    private final Object payload;
+
+    /**
+     * Caption to display as a menu entry
+     * @return
+     */
+    public String getCaption() {
+        return caption;
+    }
+
+    /**
+     * Payload to be used to distinguish which option was selected
+     * @return
+     */
+    public Object getPayload() {
+        return payload;
+    }
+
+    /**
+     * 
+     * @param caption to display as a menu entry
+     * @param payload to be used to distinguish which option was selected
+     */
+    public MenuOption(String caption, Object payload) {
+        this.caption = caption;
+        this.payload = payload;
+    }
+}
